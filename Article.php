95c66a91801b005bb1b2964ed744fffc67d0bce0
Ekaterina Sharaburova <tyf272016@gmail.com>
2022-12-18 16:58:54 +0400

    Добавлены комментарии

--- Article.php
+++ Article.php
@@ -2,7 +2,16 @@ namespace app\models;
 use Yii;
 use yii\db\ActiveRecord;
 use yii\helpers\Url;
+/**
+ * This is the model class for table "article".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ * @property int $create_time
+ * @property int $update_time
+ */
 class Article extends ActiveRecord
 {
-    
+
 }
